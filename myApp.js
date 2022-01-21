5550de2f16296d0edc9924c9efacd79a121fd5ef
Vernon Williams <voxenn@gmail.com>
2022-01-21 08:29:58 -0600

    Create json api

--- myApp.js
+++ myApp.js
@@ -3,8 +3,17 @@ var app = express();
 let absolutePath = __dirname + "/views/index.html";
 let absoluteStaticPath = __dirname + "/public";
 app.use("/public", express.static(absoluteStaticPath));
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.sendFile(absolutePath);
+});
+app.get("/json", (req, res) => {
+    res.json(
+        {
+            "name":"Vernon",
+            "career":"Developer",
+            "hobby":"Gaming"
+        }
+    );
 });
 console.log("Hello World");
 
