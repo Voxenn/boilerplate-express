c1ac42fbb4bcd120ae2046cda984b3b3f5b232fe
Vernon Williams <voxenn@gmail.com>
2022-01-21 08:18:21 -0600

    Correct path name to string.

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-let absolutePath = __dirname + /views/index.html;
+let absolutePath = __dirname + "/views/index.html";
 app.get("/", (req,res) => {
     res.sendFile(absolutePath);
 });
