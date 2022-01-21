cacb7db1b8d37ceb2812abec88a7ebb91028fc29
Vernon Williams <voxenn@gmail.com>
2022-01-21 08:23:18 -0600

    Use middleware for static css

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 let absolutePath = __dirname + "/views/index.html";
+let absoluteStaticPath = __dirname + "/public";
+app.use(path, express.static(absoluteStaticPath));
 app.get("/", (req,res) => {
     res.sendFile(absolutePath);
 });
