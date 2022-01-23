36b90b1cb39646c47e8748b9b26178eb491d814d
Vernon Williams <voxenn@gmail.com>
2022-01-23 11:04:13 -0600

    Refactor bodyParser

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var app = express();
 let absolutePath = __dirname + "/views/index.html";
 let absoluteStaticPath = __dirname + "/public";
-app.use("/public", bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use("/public", express.static(absoluteStaticPath));
 app.get("/", (req, res) => {
     res.sendFile(absolutePath);
